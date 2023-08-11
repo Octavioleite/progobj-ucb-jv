30cf12e2276e81951660384063fbaf0db1e77dc8
OCTÁVIO LEITE NETO <octavio.neto@a.ucb.br>
2023-08-11 10:38:55 -0300

    adiciona base do problema

--- sozinho/teste.java
+++ sozinho/teste.java
@@ -6,6 +6,15 @@ package sozinho;
 public class teste {
 public static void main (String args[]){
     System.out.println ("olá, mundo!");
+    verificarNumeroPrimo(123425326532623);
 }
-    
+   
+public static void verificarNumeroPrimo(Integer n ){
+
+
+}
+
+
+
+
 }
